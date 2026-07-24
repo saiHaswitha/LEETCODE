57eebcf90f8a13b628cea5c86211602518b162be
saiHaswitha <saihaswitha2007@gmail.com>
2026-07-24 15:07:54 +0530

    Time: 4 ms (80.98%) | Memory: 46.2 MB (71.20%) - LeetSync

---
+++ 928-surface-area-of-3d-shapes/surface-area-of-3d-shapes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int surfaceArea(int[][] grid) {
+
+        int area = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] > 0) {
+                    area += grid[i][j] * 4 + 2;
+                }
+
+                if (i > 0) {
+                    area -= 2 * Math.min(grid[i][j], grid[i - 1][j]);
+                }
+
+                if (j > 0) {
+                    area -= 2 * Math.min(grid[i][j], grid[i][j - 1]);
+                }
+            }
+        }
+
+        return area;
+    }
+}
